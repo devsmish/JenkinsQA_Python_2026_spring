f4707c4b581ab902ea1bc1a61182d95b59844271
ma9or <il.akhmetzhanov@yandex.kz>
2026-04-10 20:08:23 +0500

    fix class, add func test_color_picker

--- tests/check_color.py
+++ tests/check_color.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-try:
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+def test_color_picker():
+    driver = webdriver.Chrome()
+    try:
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-    driver.implicitly_wait(0.5)
+        driver.implicitly_wait(0.5)
 
-    color_picker = driver.find_element(by=By.NAME, value="my-colors")
+        color_picker = driver.find_element(by=By.NAME, value="my-colors")
 
-    current_color = color_picker.get_attribute("value")
-    print(f"Текущий цвет: {current_color}")
+        current_color = color_picker.get_attribute("value")
+        print(f"Текущий цвет: {current_color}")
 
-    assert current_color == "#563d7c", f"Ожидался #563d7c, получен {current_color}"
+        assert current_color == "#563d7c", f"Ожидался #563d7c, получен {current_color}"
 
-    print("✓ Цвет установлен корректно!")
+        print("✓ Цвет установлен корректно!")
 
-finally:
-    driver.quit()
+    finally:
+        driver.quit()
