2044e3e8a361604da70c4906b363370fa04c409c
Miguel19774 <pugbag1@gmail.com>
2026-04-11 20:34:16 +0200

    add_test_miguel

--- tests/test_miguel.py
+++ tests/test_miguel.py
@@ -6,9 +6,7 @@ def test_open_site():
     driver = webdriver.Chrome()
     driver.get("https://www.python.org")
 
-    time.sleep(5)
-
     assert "Python" in driver.title
-    time.sleep(5)
+
 
     driver.quit()
