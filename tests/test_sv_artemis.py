b154392d62b28fae0eb99a7b3352fe535f24544c
Sergey <space-researcher@i.ua>
2026-04-14 17:47:32 +0300

    My first test - fixed

--- tests/test_sv_artemis.py
+++ tests/test_sv_artemis.py
@@ -1,25 +1,23 @@
 from selenium.webdriver.common.by import By
 
-UNAME = (By.XPATH, '//*[@id="loginPanel"]/form/div[1]/input')
-PWD = (By.XPATH,'//*[@id="loginPanel"]/form/div[2]/input')
-BTN = (By.XPATH,'//*[@id="loginPanel"]/form/div[3]/input')
+USERNAME = (By.XPATH, '//input[@name="username"]')
+PASSWORD = (By.XPATH, '//input[@name="password"]')
+BUTTON = (By.XPATH, "//input[@value='Log In']")
 
 
 def test_input_admin(browser):
     """Негативный тест логина в админ-панель"""
-    browser.implicitly_wait(2)
+    browser.implicitly_wait(2)   # это общее ожидание для всех browser.find_element()
     browser.get("https://parabank.parasoft.com/parabank/admin.htm")
 
-    text_box1 = browser.find_element(*UNAME)
-    text_box2 = browser.find_element(*PWD)
-    submit_button = browser.find_element(*BTN)
+    text_box1 = browser.find_element(*USERNAME)
+    text_box2 = browser.find_element(*PASSWORD)
+    submit_button = browser.find_element(*BUTTON)
 
     text_box1.send_keys("Selenium")
     text_box2.send_keys("Test")
     submit_button.click()
 
-    element = browser.find_element(By.CSS_SELECTOR, "#rightPanel > p")
-    text = element.text
-    print('\n', text)
+    element_text = browser.find_element(By.CSS_SELECTOR, "#rightPanel > p").text
 
-    assert "could not be verified" in text
+    assert "could not be verified" in element_text
