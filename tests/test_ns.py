72d04af46f6ecd5b50cdff355e143654206b830a
NataliiaSerkes <serkes.natasha@gmail.com>
2026-04-15 17:56:52 +0200

    My first test

---
+++ tests/test_ns.py
@@ -0,0 +1,20 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def test_python_org_search_input(browser):
+    """Тест заполнения поля поиска"""
+    browser.get("https://www.python.org/")
+
+    search_box = browser.find_element(By.ID, "id-search-field")
+    search_box.clear()
+    search_box.send_keys("Job - Python Developer")
+
+    search_box.send_keys(Keys.ENTER)
+
+
+    time.sleep(1)  # Пауза для наглядности
+
+    assert "Search" in browser.find_element(By.XPATH,'//*[@id="content"]/div/section/h2').text
+    print("✓ Тест пройден успешно!")
