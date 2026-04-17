c995016d4518024853cc87fd5b195ee3f8109f36
Sergey.D <sergei.demyanenko@gmail.com>
2026-04-16 22:58:17 -0400

    Test refactoring

--- tests/test_jenkins_example.py
+++ tests/test_jenkins_example.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
 
-def test_python_org_search_input(browser):
+def test_welcome(browser):
     label = browser.find_element(By.CSS_SELECTOR, "div.empty-state-block > h1")
 
     assert label.text == "Welcome to Jenkins!"
