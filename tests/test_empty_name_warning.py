e8c3c5510147466cc0639458b603b34702840996
Miguel19774 <pugbag1@gmail.com>
2026-04-19 19:00:24 +0200

    add negative test for empty item name

---
+++ tests/test_empty_name_warning.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+def test_empty_item_mame(browser):
+    element_button = browser.find_element(By.XPATH, '//a[contains(., "New Item")]')
+    element_button.click()
+    assert "/view/all/newJob" in browser.current_url
+    element = browser.find_element(By.ID,"name")
+    assert element.get_attribute("value") == ""
+    folder = browser.find_element(By.XPATH,"//li[contains(@class, 'folder_Folder')]")
+    folder.click()
+    ok_button =browser.find_element(By.ID,"ok-button")
+    warning_message = browser.find_element(By.XPATH,"//div[@id='itemname-required']")
+    assert warning_message.is_displayed()
