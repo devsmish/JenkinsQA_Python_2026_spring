e9ea9529d10bae707131dd0d38dd5812bfda7897
Miguel19774 <pugbag1@gmail.com>
2026-04-19 19:18:45 +0200

    fix test_ok_button_enabled_for_folder

--- tests/test_folder_ok_button.py
+++ tests/test_folder_ok_button.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 def test_ok_button_enabled_for_folder(browser):
     element_button = browser.find_element(By.XPATH, '//a[contains(., "New Item")]')
     element_button.click()
-    assert "/view/all/newJob" in browser.current_url
     element = browser.find_element (By.XPATH, "//input[@name='name']")
     element.send_keys("TestFolder")
     folder = browser.find_element(By.XPATH, "//li[contains(@class,'folder_Folder')]")
     folder.click()
     ok_button = browser.find_element(By.ID, "ok-button")
+    assert "/view/all/newJob" in browser.current_url
     assert ok_button.is_displayed()
