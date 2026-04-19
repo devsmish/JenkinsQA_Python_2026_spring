b69c89b8ad67497175c2ae41b89cf4e545998638
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-04-19 16:10:03 +0300

    TC_04.001.02 | Multi-configuration project Configuration > Disable the Project> Verify that toggle has tooltip

--- tests/test_multi_configuration_project.py
+++ tests/test_multi_configuration_project.py
@@ -1,3 +1,6 @@
+import time
+
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -20,3 +23,21 @@ def test_verify_status_switching_enable_button(browser):
         EC.visibility_of_element_located((By.ID, "enable-project"))).text
 
     assert "This project is currently disabled" in actual_disable_text
+
+
+def test_verify_enable_toogle_has_tooltip(browser):
+    browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
+
+    browser.find_element(By.ID, "name").send_keys(multiconfiguration_project_name)
+    browser.find_element(By.CLASS_NAME, "hudson_matrix_MatrixProject").click()
+    browser.find_element(By.ID, "ok-button").click()
+
+    enabled_toogle = browser.find_element(By.ID, "toggle-switch-enable-disable-project")
+
+    actions = ActionChains(browser)
+    actions.move_to_element(enabled_toogle).perform()
+
+    toggle_tooltip = WebDriverWait(browser, 5).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "tippy-content"))).text
+
+    assert toggle_tooltip == "Enable or disable the current project"
