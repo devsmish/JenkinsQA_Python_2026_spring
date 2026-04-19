712fffcc0348e3aed13db2f25998d4baa8e730dc
NataliiaSerkes <serkes.natasha@gmail.com>
2026-04-19 17:50:32 +0200

    Edd new test and mooving old test

--- tests/test_jenkins_ns.py
+++ tests/test_jenkins_ns.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.by import By
 def test_verify_enable_toogle_has_tooltip(browser):
     browser.find_element(By.XPATH, "//[@href='newJob']").click()
 
-    browser.find_element(By.ID, "name").send_keys(test_1)
-    browser.find_element(By.XPATH, '//[@class=org_jenkinsci_plugins_workflow_job_WorkflowJob]').click()
+    browser.find_element(By.ID, "name").send_keys("test_1")
+    browser.find_element(By.XPATH, "//[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']").click()
     browser.find_element(By.ID, "ok-button").click()
 
     browser.find_element(By.ID, "name='Submit").click()
-
+    label=browser.find_element(By.ID,"job-index-headline page-headline")
 
     assert label.text == "test_1"
