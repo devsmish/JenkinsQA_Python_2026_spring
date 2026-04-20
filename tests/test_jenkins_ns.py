62bfbaa2989720c3b7cb4ba9648a892f3af9063b
NataliiaSerkes <serkes.natasha@gmail.com>
2026-04-20 22:41:33 +0200

    Edd new test ns

--- tests/test_jenkins_ns.py
+++ tests/test_jenkins_ns.py
@@ -7,7 +7,7 @@ def test_jenkins_ns(browser):
     browser.find_element(By.ID, "name").send_keys("test_1")
     browser.find_element(By.XPATH,"//*[@id='j-add-item-type-standalone-projects']/ul/li[1]").click()
     browser.find_element(By.ID, "ok-button").click()
-    time.sleep(5)
+    time.sleep(6)
     browser.find_element(By.XPATH, "//*[@id='bottom-sticker']/div/button[1]").click()
     time.sleep(5)
     label=browser.find_element(By.XPATH, "//*[@id='main-panel']/div[1]/div[1]/h1")
