0670ac28e4e65c8c3c127fcf68714637e656d7b5
Miguel19774 <pugbag1@gmail.com>
2026-04-22 20:03:08 +0200

    add test_jenkins_plugins_available_plugins_search

--- tests/test_manage_jenkins_plugins_available_plugins_search.py
+++ tests/test_manage_jenkins_plugins_available_plugins_search.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
+
 def test_jenkins_plugins_available_plugins_search(browser):
     icon_setting = browser.find_element(By.XPATH, '//a[@id="root-action-ManageJenkinsAction"]')
     icon_setting.click()
