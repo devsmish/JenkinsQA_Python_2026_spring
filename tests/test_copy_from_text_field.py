2f2b161787f26d6df3d83d6b1acda690f3a0e469
taras <tarsic99@gmail.com>
2026-04-21 23:59:55 -0400

    More of flaky test fixing.

--- tests/test_copy_from_text_field.py
+++ tests/test_copy_from_text_field.py
@@ -1,11 +1,13 @@
-from time import sleep
-
-import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
-@pytest.mark.skip()
-def test_copy_from_text_field(browser):
-    """TC_01.003.01. Verifies 'Copy from' Text Field available"""
+
+def create_new_pipeline(browser):
+
+    driver = browser
+    wait = WebDriverWait(driver, 10)
+
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
     browser.find_element(By.XPATH, "//input[@id='name']").send_keys("my_first_pipeline")
     browser.find_element(By.XPATH, "(//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob'])[1]").click()
@@ -13,16 +15,30 @@ def test_copy_from_text_field(browser):
     browser.find_element(By.XPATH, "(//textarea[@name='description'])[1]").send_keys("My first pipeline")
     browser.maximize_window()
     browser.execute_script("window.scrollBy({ top: 1200, left: 0, behavior: 'auto' });")
-    sleep(1)
-    browser.find_element(By.XPATH, "(//button[normalize-space()='Save'])[1]").click()
-    browser.find_element(By.XPATH, "(//a[@class='app-jenkins-logo'])[1]").click()
+    wait.until(ec.element_to_be_clickable((By.XPATH, "(//button[normalize-space()='Save'])[1]"))).click()
+
+
+def test_copy_from_text_field(browser):
+    """TC_01.003.01. Verifies 'Copy from' Text Field available"""
+
+    driver = browser
+    wait = WebDriverWait(driver, 10)
+
+    create_new_pipeline(browser)
+
+    browser.find_element(By.CSS_SELECTOR, "#jenkins-head-icon").click()
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
     browser.execute_script("window.scrollBy({ top: 800, left: 0, behavior: 'auto' });")
-    sleep(1)
-    text_field = browser.find_element(By.XPATH, "(//label[normalize-space()='Copy from'])[1]")
-    browser.find_element(By.XPATH, "(//input[@id='from'])[1]").click()
-    browser.find_element(By.XPATH, "(//input[@id='from'])[1]").send_keys("my_first_pipeline")
+    wait.until(ec.visibility_of_element_located((By.XPATH, "(//label[normalize-space()='Copy from'])[1]")))
 
-    assert text_field.text == "Copy from"
+    text_field_name = browser.find_element(By.XPATH, "(//label[normalize-space()='Copy from'])[1]")
+    assert text_field_name.text == "Copy from"
 
-#This is just a comment to introduce a change to allow new commit and push.
+    text_field = browser.find_element(By.XPATH, "(//input[@id='from'])[1]")
+    text_field.click()
+    text_field.send_keys("my_first_pipeline")
+
+    typed_text = text_field.get_attribute("value")
+    assert typed_text == "my_first_pipeline"
+
+    # print("\n" f"All good, TC_01.003.01 passed, the text is: {typed_text}")
