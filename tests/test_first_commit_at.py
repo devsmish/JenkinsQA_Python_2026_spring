02303f3165167d7e6a491da95ebfc5aeafd60376
Tanya <akulenko.t.v.18@gmail.com>
2026-04-22 16:58:40 +0300

    Тест проверки текстов на главной странице Проба коммита

--- tests/test_first_commit_at.py
+++ tests/test_first_commit_at.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 def test_text_main_page(browser):
-    """Тест проверки текстов на главной странице"""
+    """Тест проверки текстов на главной странице
+       Тест работы с гитом"""
 
     label = browser.find_element(By.CSS_SELECTOR, "div.empty-state-block > h1")
     assert label.text == "Welcome to Jenkins!"
