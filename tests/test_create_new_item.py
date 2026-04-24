c04d47719dc3b38c9a7fb346b52838c6a635d820
Andrii Koss <sqakross@gmail.com>
2026-04-23 21:22:47 -0700

    TC_01.001.03 update code

--- tests/test_create_new_item.py
+++ tests/test_create_new_item.py
@@ -3,23 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.mark.skip
 @pytest.mark.parametrize("invalid_char", ["?", "*", "/", "|", "!", "%", "&", ";", ":"])
 def test_create_new_item_validate_unsupported_special_characters(browser, invalid_char):
     wait = WebDriverWait(browser, 10)
 
     browser.find_element(By.LINK_TEXT, "New Item").click()
+    browser.find_element(By.ID, "name").send_keys(f"test{invalid_char}job")
 
-    name_input = wait.until(EC.visibility_of_element_located((By.ID, "name")))
-    name_input.clear()
-    name_input.send_keys(f"test{invalid_char}job")
-
-    wait.until(
-        EC.text_to_be_present_in_element((By.ID, "itemname-invalid"), "unsafe character")
+    """need wait because the element exists in DOM but is not visible yet, while"""
+    warning = wait.until(
+        EC.visibility_of_element_located((By.ID, "itemname-invalid"))
     )
-    warning = browser.find_element(By.ID, "itemname-invalid")
     ok_button = browser.find_element(By.ID, "ok-button")
 
-    assert warning.is_displayed()
     assert "unsafe character" in warning.text
     assert ok_button.get_attribute("disabled") is not None
