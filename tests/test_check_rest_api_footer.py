0fc531f7e15fc226b78e79c79ed51dd33325e354
zevs-wot <zevs.wot.zevs@mail.ru>
2026-04-26 12:47:23 +0400

    Refactoring test: delete all comments and code change

--- tests/test_check_rest_api_footer.py
+++ tests/test_check_rest_api_footer.py
@@ -1,44 +1,28 @@
-import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
-# Локаторы
-REST_API_LINK = (By.XPATH, '//footer//a[text()="REST API"]')
-MANAGE_JENKINS_BUTTON = (By.ID, 'root-action-ManageJenkinsAction')
-NODES_LINK = (By.CSS_SELECTOR, 'a[href="computer"]')
-CREDENTIALS_LINK = (By.CSS_SELECTOR, 'a[href*="credentials"]')
+def _get_rest_api_link(driver):
+    locator = (By.XPATH, '//footer//a[text()="REST API"]')
+    return driver.find_element(*locator)
 
 
-def check_rest_api_visible(driver):
-    """Проверка наличия ссылки 'REST API' в футере страницы"""
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(REST_API_LINK)
-    )
-
-    assert element.is_displayed()
-    assert element.text == "REST API"
-
-@pytest.mark.smoke
-def test_rest_api_on_dashboard(browser):
-    """Проверка наличия отображения 'REST API' на главной странице"""
-    check_rest_api_visible(browser)
+def _get_manage_jenkins_button(driver):
+    manage_jenkins_button = (By.ID, 'root-action-ManageJenkinsAction')
+    return driver.find_element(*manage_jenkins_button)
 
 
-@pytest.mark.smoke
-def test_rest_api_on_nodes(browser):
-    """Проверка наличия отображения 'REST API' на странице 'Nodes'"""
-    browser.find_element(*MANAGE_JENKINS_BUTTON).click()
-    browser.find_element(*NODES_LINK).click()
+def test_checking_the_button_on_key_pages(browser):
+    rest_api_link = _get_rest_api_link(browser)
+    assert rest_api_link.is_displayed()
 
-    check_rest_api_visible(browser)
+    _get_manage_jenkins_button(browser).click()
+    browser.find_element(By.CSS_SELECTOR, 'a[href="computer"]').click()
 
+    rest_api_link = _get_rest_api_link(browser)
+    assert rest_api_link.is_displayed()
 
-@pytest.mark.smoke
-def test_rest_api_on_credentials(browser):
-    """Проверка наличия отображения 'REST API' на странице 'Credentials'"""
-    browser.find_element(*MANAGE_JENKINS_BUTTON).click()
-    browser.find_element(*CREDENTIALS_LINK).click()
+    _get_manage_jenkins_button(browser).click()
+    browser.find_element(By.CSS_SELECTOR, 'a[href="credentials"]').click()
 
-    check_rest_api_visible(browser)
+    rest_api_link = _get_rest_api_link(browser)
+    assert rest_api_link.is_displayed()
