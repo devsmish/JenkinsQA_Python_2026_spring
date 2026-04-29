7da626c6c416e53650ff410a10abf121fae2f78d
zevs-wot <zevs.wot.zevs@mail.ru>
2026-04-29 22:12:08 +0400

    add test checking the dropdown list when entering one letter

--- tests/test_settings_search_field.py
+++ tests/test_settings_search_field.py
@@ -13,4 +13,6 @@ def test_checking_the_dropdown_when_entering_one_letter(browser):
     items = browser.find_elements(By.CSS_SELECTOR, '.jenkins-search__results-container--visible a')
 
     assert len(items) > 1
-    assert all('t' in item.text.lower() for item in items)
+
+    for item in items:
+        assert 't' in item.text.lower(), f"Элемент {item.text} не содержит 't'"
