bd764fe3406afbb470c2626cfae5c9dd55541d03
Irina Bibikova <irinabibikova1985@gmail.com>
2026-04-30 22:37:28 +0100

    add comments

--- tests/test_manage_page.py
+++ tests/test_manage_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# TC_10.001.01 | Manage Jenkins > Visibility and clickability > Verify navigation to manage Jenkins page
 def test_verify_navigation_to_manage_page(browser):
     wait = WebDriverWait(browser, 10)
 
@@ -13,7 +14,7 @@ def test_verify_navigation_to_manage_page(browser):
     assert "/manage" in browser.current_url
 
 
-
+# TC_10.001.02 | Manage Jenkins > Visibility and clickability > Verify "Manage Jenkins" option is visible on Dashboard
 def test_manaje_jenkins_icon_is_visible(browser):
     wait = WebDriverWait(browser, 10)
 
