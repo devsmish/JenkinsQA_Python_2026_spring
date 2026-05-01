1e22c453adec1ed8377d11abcea1d2242a90c64f
zevs-wot <zevs.wot.zevs@mail.ru>
2026-05-01 16:25:59 +0400

    1. add test checking clear search field 2. moved the variable into a function for all tests

--- tests/test_settings_search_field.py
+++ tests/test_settings_search_field.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from conftest import browser
 
@@ -6,8 +7,12 @@ def _get_field_search(driver):
     return driver.find_element(By.ID, 'settings-search-bar')
 
 
+def _get_manage_jenkins_btn(driver):
+    return driver.find_element(By.ID, 'root-action-ManageJenkinsAction')
+
+
 def test_checking_the_dropdown_when_entering_one_letter(browser):
-    browser.find_element(By.ID, 'root-action-ManageJenkinsAction').click()
+    _get_manage_jenkins_btn(browser).click()
 
     _get_field_search(browser).send_keys('t')
     items = browser.find_elements(
@@ -19,7 +24,7 @@ def test_checking_the_dropdown_when_entering_one_letter(browser):
 
 
 def test_checking_the_dropdown_when_entering_full_name_settings(browser):
-    browser.find_element(By.ID, 'root-action-ManageJenkinsAction').click()
+    _get_manage_jenkins_btn(browser).click()
 
     _get_field_search(browser).send_keys('Security')
     item = browser.find_element(
@@ -28,3 +33,18 @@ def test_checking_the_dropdown_when_entering_full_name_settings(browser):
     )
 
     assert item.text.strip() == 'Security'
+
+
+def test_clear_search_field_via_keyboard_and_button(browser):
+    _get_manage_jenkins_btn(browser).click()
+    field = _get_field_search(browser)
+
+    field.send_keys('System')
+    field.send_keys(Keys.CONTROL + 'a')
+    field.send_keys(Keys.BACK_SPACE)
+
+    field.send_keys('Manage Jenkins')
+    field.clear()
+
+    assert field.get_attribute('value') == ''
+    assert 'No results' in browser.find_element(By.CSS_SELECTOR, '.jenkins-search__results-container').text
