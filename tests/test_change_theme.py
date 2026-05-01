23f04cfce4fc9957ec2c52d80f136a4bd181110f
Viktor.Kashchcenko <lordpeka9@gmail.com>
2026-05-01 21:05:49 +0300

    add new test in test_change_theme.py

--- tests/test_change_theme.py
+++ tests/test_change_theme.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-
+standard_themes_values = {
+        'dark': 'dark',
+        'light': 'none',
+        'system': 'system'
+    }
 
 def test_quick_change_theme_not_able(browser):
     ActionChains(browser).move_to_element(browser.find_element(By.ID, 'root-action-UserAction')).perform()
@@ -9,3 +13,13 @@ def test_quick_change_theme_not_able(browser):
 
     ActionChains(browser).move_to_element(browser.find_element(By.ID, 'root-action-UserAction')).perform()
     assert not browser.find_elements(By.ID, 'account-theme-picker'), "Error: Quick theme change block is displayed, when the Appearance page is open"
+
+
+def test_select_system_theme_by_quick_change(browser):
+
+    ActionChains(browser).move_to_element(browser.find_element(By.ID, 'root-action-UserAction')).perform()
+
+    locator = f'#account-theme-picker > option[value *= "{standard_themes_values.get("system")}"]'
+    browser.find_element(By.CSS_SELECTOR, locator).click()
+
+    assert 'System' in browser.find_element(By.CSS_SELECTOR, 'label[for="account-theme-picker"] > span').text
