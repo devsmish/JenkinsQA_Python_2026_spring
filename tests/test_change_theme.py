48c73803f924ff06d39b0a5e4bcce7bd21400633
Viktor.Kashchcenko <lordpeka9@gmail.com>
2026-05-01 20:22:10 +0300

    Fix CI error in profile icon locator

--- tests/test_change_theme.py
+++ tests/test_change_theme.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 def test_quick_change_theme_not_able(browser):
-    ActionChains(browser).move_to_element(browser.find_element(By.XPATH, '//a[@href="/user/admin"]')).perform()
+    ActionChains(browser).move_to_element(browser.find_element(By.ID, 'root-action-UserAction')).perform()
     browser.find_element(By.CSS_SELECTOR, 'a.jenkins-dropdown__item[href $= "appearance"]').click()
 
-    ActionChains(browser).move_to_element(browser.find_element(By.XPATH, '//a[@href="/user/admin"]')).perform()
+    ActionChains(browser).move_to_element(browser.find_element(By.ID, 'root-action-UserAction')).perform()
     assert not browser.find_elements(By.ID, 'account-theme-picker'), "Error: Quick theme change block is displayed, when the Appearance page is open"
