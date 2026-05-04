806255e900635a6c7ea4e17e0f2ef7959463f777
Sergei Krivonosov <ksva690@gmail.com>
2026-05-04 11:21:20 +0200

    add dependecy tests

--- tests/test_folder.py
+++ tests/test_folder.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 FOLDER_NAME = "TestFolder"
 SECOND_FOLDER_NAME = "SecondFolder"
@@ -56,7 +57,7 @@ def test_create_folder_with_description(browser):
 
     assert browser.find_element(By.ID, "view-message").text == description
 
-
+@pytest.mark.skip
 def test_create_new_folder(browser):
     name = "new_folder"
 
@@ -137,3 +138,63 @@ def test_create_folder_with_same_name_in_different_parent(browser):
     create_folder(browser, SECOND_FOLDER_NAME)
 
     assert browser.find_element(By.CLASS_NAME, "job-index-headline").text == SECOND_FOLDER_NAME
+
+
+@pytest.mark.dependency()
+def test_create_test_folder(browser):
+    wait = WebDriverWait(browser, 5)
+
+    wait.until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "New Item"))).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="name"]'))).send_keys('My first folder')
+
+    wait.until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'com_cloudbees_hudson_plugins_folder_Folder'))).click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'ok-button'))).click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//button[@value="Save"]'))).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//h1[text()="My first folder"]')))
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'jenkins-head-icon'))).click()
+
+    new_folder = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'My first folder')))
+    assert new_folder.is_displayed()
+
+@pytest.mark.dependency(depends=['test_create_test_folder'])
+def test_create_folder_from_copy(browser):
+    wait = WebDriverWait(browser, 5)
+
+    wait.until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "New Item"))).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.ID, 'name'))).send_keys('Folder from copy')
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'from'))).send_keys('My first folder')
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'from'))).send_keys(Keys.ENTER)
+
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//button[@value="Save"]'))).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//h1[text()="Folder from copy"]')))
+
+    wait.until(
+        EC.presence_of_element_located((By.ID, 'jenkins-head-icon')))
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'jenkins-head-icon'))).click()
+
+    folder = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Folder from copy')))
+    assert folder.is_displayed()
