f7af3c2633c774487d714417c2e5f61aa6fd3db4
NataliiaSerkes <serkes.natasha@gmail.com>
2026-05-06 22:44:20 +0200

    The new dependency test is added

--- tests/test_pipeline_project2.py
+++ tests/test_pipeline_project2.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+@pytest.mark.dependency()
 def test_create_pipeline_project(browser):
-    test="test_1"
-    wait = WebDriverWait(browser, 10)
+    pipeline_name = "test_1"
+    wait = WebDriverWait(browser, 5)
 
     browser.find_element(By.LINK_TEXT, "New Item").click()
-    browser.find_element(By.ID, "name").send_keys(test)
+    browser.find_element(By.ID, "name").send_keys(pipeline_name)
 
     browser.find_element(By.XPATH,"//ul/li//span[text()='Pipeline']").click()
     browser.find_element(By.ID, "ok-button").click()
@@ -20,5 +20,18 @@ def test_create_pipeline_project(browser):
     browser.find_element(By.XPATH, "//*[@class='app-jenkins-logo']").click()
 
     label=wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@href='job/test_1/']"))).text
-    assert label == test
+    assert label == pipeline_name
 
+@pytest.mark.dependency(depends=["test_create_pipeline_project"])
+def test_add_description_pipeline(browser):
+    text_description = "Description here"
+    wait = WebDriverWait(browser, 5)
+
+    browser.find_element(By.LINK_TEXT, "test_1").click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[normalize-space()='Configure']"))).click()
+
+    browser.find_element(By.NAME, "description").send_keys(text_description)
+
+    browser.find_element(By.NAME, "Submit").click()
+
+    assert browser.find_element (By.ID, "description-content").text == text_description
